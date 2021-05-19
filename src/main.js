fdd2f7fd6c9ba9aea7d999173c0a62ba30bc4909
svetanguyen <matematicaforeber@gmail.com>
2021-05-19 22:59:11 +0700

    config amplify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue';
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
 
 Vue.config.productionTip = false
 
