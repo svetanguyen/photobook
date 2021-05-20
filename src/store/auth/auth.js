d1baa6926484ef2a5336dcc19f6150df483a1b84
svetanguyen <matematicaforeber@gmail.com>
2021-05-20 22:57:08 +0700

    set up authentification

---
+++ src/store/auth/auth.js
@@ -0,0 +1,64 @@
+import {Auth, loadingContainer} from 'aws-amplify';
+
+export const auth = {
+    namespace: true,
+    state: { user: null },
+    mutations: {
+        setUser(state, payload) {
+            state.user = payload;
+        }
+    },
+    actions: {
+        async logout({commit}) {
+            commit("setUser", null);
+            return await Auth. signOut();
+        },
+        async loadingContainer({commit, dispatch}, {username, password}) {
+            try {
+                await Auth.signIn({
+                    username,
+                    password
+                });
+
+                const userInfo = await Auth.currentUserInfo();
+                commit("setUser", userInfo)
+                return  Promise.resolve("Success");
+            } catch (error) {
+                console.log(error);
+                return  Promise.reject(error);
+            }
+        },
+        async confirmSignUp(_, {username, code}) {
+            try {
+                await Auth.confirmSignUp(username, code);
+                return Promise.resolve('Success');
+            } catch (error) {
+                console.log(error);
+                return Promise.reject(error);
+            }
+        },
+        async signUp(_, {username, password, email}) {
+            try {
+                await Auth.signUp({
+                    username,
+                    password,
+                    attributes: {
+                        email
+                    }
+                })
+                return Promise.resolve('Success');
+            } catch (error) {
+                console.log(error);
+                return Promise.reject(error);
+            }
+        },
+        async authAction({commit}) {
+            const userInfo = await Auth.currentUserInfo();
+            commit("setUser", userInfo);
+
+        }
+    },
+    getters: {
+        user: (state) => state.user
+    }
+}
