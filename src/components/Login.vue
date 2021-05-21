b8cd120829f01b1e46c5a6cb7ddd672bc680ca1d
svetanguyen <matematicaforeber@gmail.com>
2021-05-21 23:34:43 +0700

    login methods

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,8 +3,29 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
 export default {
-    
+    data: () => ({
+        username: "",
+        password: "",
+        email: "",
+        error: ""
+    }),
+    methods: {
+        ...mapActions({
+            loginVue: "auth/login"
+        }),
+        async login() {
+            try {
+                await this.loginVue({
+                    username: this.username,
+                    password: this.password
+                })
+            } catch (error) {
+                this.error = error;
+            }
+        }
+    }
 }
 </script>
 
