3e79f69cb7106ba7c767a9beb1753365e8bc5e0e
svetanguyen <matematicaforeber@gmail.com>
2021-05-23 19:52:52 +0700

    methods for signup

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -15,6 +15,45 @@ export default {
         error: '',
         confirmPassword: '',
         code: ''
-    })
+    }),
+    methods: {
+        async signUp() {
+            if(!this.email || !this.password) {
+                return;
+            }
+            try {
+                await this.$store.dispatch('auth/signUp', {
+                    username: this.username,
+                    password: this.password,
+                    email: this.email
+                });
+
+                this.confirmPassword = true;
+            } catch(error) {
+                this.error = error;
+            }
+        },
+        async confirmSignUp() {
+            if (!this.username || !this.code) {
+                return;
+            }
+
+            try {
+                await this.$store.dispatch('auth/confirmSignUp', {
+                    username: this.username,
+                    code: this.code
+                });
+                await this.$store.dispatch('authlogin', {
+                    username: this.username,
+                    password: this.password
+                });
+                this.$router.push('/albums');
+
+            } catch(error) {
+                console.log(error);
+                this.error = error;
+            }
+        }
+    }
 }
 </script>
